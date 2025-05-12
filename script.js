e20db46e8ae829bc04c554833e7e32e325772bec
iamayac <ayachristia@gmail.com>
2025-05-12 09:35:04 +0200

    done

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-// løs opgaverne her
+// opgave 1) DESTRUKTURERINR OBEJCTS AMD ARRAYS
+let [, dog1, dog2] = dogs
+console.log('- hund1;', dog1, '- hund2;', dog2);
+// opgave 2)
+// let navn = cat.name;
+// let race = cat.breed;
+// let ownersEmail = cat.owner.contact
+// let { name, breed, owner: { contact } } = cat;
+// console.log('- Navn;', name, '- Race;', breed, '-Email;', contact);
+// opgave 3)
+function destructureToy({ name, favoriteToys }) {
+    console.log(name, ...favoriteToys);
+}
+destructureToy(cat)
+
+// --------------------------------------------------------
+
+// opgave 4) REST
+let [dogA, dogB, ...otherDogs] = dogs;
+console.log(dogA, dogB, otherDogs);
+// opgave 5)
+let dogsWithC = otherDogs.filter((dog) => dog.startsWith("C"))
+console.log(dogsWithC);
+// opgave 6)
+let { breed, ...otherInformation } = cat;
+console.log(otherInformation);
+
+// --------------------------------------------------------
+// opgave 7) SPREAD OPERATOR
+let rabbitBreeds = [...smallRabbitBreeds, ...largeRabbitBreeds]
+console.log(rabbitBreeds);
+// opgave 8)
+let checkeredGiantInfo = {
+    ...checkeredGiantBasic,
+    ...checkeredGiantTraits
+}
+console.log(checkeredGiantInfo);
+
+// --------------------------------------------------------
+// opgave 9) OPTIONAL CHAINING
+westernMovies.forEach((movie) => {
+    console.log(movie.details?.director ?? '!has no director!');
+})
+// opgave 10)
+let user = 'Aya';
+user ? console.log('Velkommen', user) : console.log('You are not logged in');
